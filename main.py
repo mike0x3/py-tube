b5249d7caaf45df20591be1d94139eb6a11c90a6
Uno qualunque <tropea2004@gmail.com>
2021-03-31 13:05:18 +0200

    Add existing file

--- main.py
+++ main.py
@@ -1,17 +1,64 @@
-from flask import Flask, render_template, request, url_for
-import os 
+from flask import Flask, render_template, request, url_for, flash
+from datetime import datetime
 import time
+import os
+import smtplib
+import threading
 from pytube import YouTube
 
 
-#yt.streams.first().download()
-
 app = Flask(__name__)
+app.secret_key = 'qwdiOKLWF2342JKEd'
+def delete_file(name):
+	direction = '/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/'
+	time.sleep(20)
+	os.remove(f'{direction}{name}')
 
-
-@app.route('/')
-def index():
-	return render_template('index.html')
+@app.route('/', methods=['POST', 'GET'])
+def index():	
+	if request.method == "POST":
+		variable_i = open('variable.txt', 'r')
+		i = int(variable_i.read())
+		variable_i.close()
+		link = request.form['link']
+		yt = YouTube(link)
+		titolo = yt.title
+		descrizione = yt.description
+		preview = yt.thumbnail_url
+		try:
+			if request.form['video'] == 'Scarica video':
+				yt.streams.filter(file_extension='mp4').first().download('/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos')
+				file_name = f'test{i}.mp4'
+				os.rename(f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/{titolo}.mp4', f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/test{i}.mp4')
+				variable_i = open('variable.txt', 'w')
+				variable_i.write(str(i+1))
+				variable_i.close()
+				threading.Thread(target=delete_file, name="Thread1", args=[file_name]).start()
+				flash('File in arrivo')
+				return render_template('index.html', titolo=titolo, descrizione=descrizione, preview=preview, file_name=file_name)
+		except Exception:
+			pass
+	
+		if request.form['audio'] == 'Scarica audio':
+			gay_list = yt.streams.filter(only_audio=True).first()
+			yt.streams.filter(only_audio=True).first().download('/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos')
+			extension = str(gay_list)
+			ext = extension[37:41]
+			if ext == "webm":
+				print(ext)
+				file_name = f'test{i}.{ext}'
+			else: 
+				ext = extension[37:40]
+				file_name = f'test{i}.{ext}'
+			os.rename(f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/{titolo}.{ext}', f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/test{i}.mp4')
+			variable_i = open('variable.txt', 'w')
+			variable_i.write(str(i+1))
+			variable_i.close()
+			threading.Thread(target=delete_file, name="Thread1", args=[file_name]).start()
+			flash('File in arrivo')
+			return render_template('index.html', titolo=titolo, descrizione=descrizione, preview=preview, file_name=file_name)
+	else:
+		return render_template('index.html')
 
 @app.route('/download', methods=['GET', 'POST'])
 def yt_download():
@@ -23,22 +70,38 @@ def yt_download():
 	titolo = yt.title
 	descrizione = yt.description
 	preview = yt.thumbnail_url
+	try:
+		if request.form['video'] == 'Scarica video':
+			yt.streams.filter(file_extension='mp4').first().download('/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos')
+			file_name = f'test{i}.mp4'
+			os.rename(f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/{titolo}.mp4', f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/test{i}.mp4')
+			variable_i = open('variable.txt', 'w')
+			variable_i.write(str(i+1))
+			variable_i.close()
+			threading.Thread(target=delete_file, name="Thread1", args=[file_name]).start()
+			return render_template('download.html', titolo=titolo, descrizione=descrizione, preview=preview, file_name=file_name)
+	except Exception:
+		pass
 	
-	
-	yt.streams.filter(file_extension='mp4').first().download('/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos')
-	file_name = f'test{i}.mp4'
-	os.rename(f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/{titolo}.mp4', f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/test{i}.mp4')
-	
-	
-	variable_i = open('variable.txt', 'w')
-	variable_i.write(str(i+1))
-	variable_i.close()
-	return render_template('download.html', titolo=titolo, descrizione=descrizione, preview=preview, file_name=file_name)
+	if request.form['audio'] == 'Scarica audio':
+		gay_list = yt.streams.filter(only_audio=True).first()
+		yt.streams.filter(only_audio=True).first().download('/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos')
+		extension = str(gay_list)
+		ext = extension[37:41]
+		if ext == "webm":
+			print(ext)
+			file_name = f'test{i}.{ext}'
+		else: 
+			ext = extension[37:40]
+			file_name = f'test{i}.{ext}'
+		os.rename(f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/{titolo}.{ext}', f'/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/test{i}.mp4')
+		variable_i = open('variable.txt', 'w')
+		variable_i.write(str(i+1))
+		variable_i.close()
+		threading.Thread(target=delete_file, name="Thread1", args=[file_name]).start()
+		return render_template('download.html', titolo=titolo, descrizione=descrizione, preview=preview, file_name=file_name)
 
-def delete_file(name):
-	direction = '/Users/nontuopc01/Desktop/PyTube [TEST] V0.1 ALPHA/static/videos/'
-	time.sleep(12000000)
-	os.remove(f'{direction}{name}')
+	
 
 @app.route('/contact', methods=["POST", "GET"])
 def contattaci():
@@ -61,6 +124,7 @@ def contattaci():
 		server.login(sender_email, sender_password)
 		server.sendmail(sender_email, receiver_email, email_text)
 		flash('La sua domanda é stata inviata ai nostri admin!')
+		return render_template('contact.html')
 	return render_template('contact.html')
 
 
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -7,3 +7,22 @@ function toTutorial(){
 	var tut = document.getElementById("toTutorial");
 	tut.scrollIntoView();
 }
+
+//adaptive mobile header
+function mobileNavbar() {
+	var el = document.getElementById("header-right");
+	if (el.style.display === "flex") {
+    el.classList.toggle("mobile-visible");
+    } else {
+    el.classList.toggle("mobile-visible");
+    el.style.animation = "animatenavbar 0.3s";
+    }
+}
+
+function toTop() {
+	window.scrollTo(0,0);
+}
+
+function navbarAnimation(x) {
+		x.classList.toggle("change");
+}
